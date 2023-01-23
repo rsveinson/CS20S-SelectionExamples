cf4ef2b2d2cddf0281bab8f921cd7f82ad9d362f
rsveinson <rsveinson@mts.net>
2023-01-23 08:59:08 -0600

    added try catch to starter code

--- JusticeLeagueDuesStart.java
+++ JusticeLeagueDuesStart.java
@@ -1,6 +1,5 @@
 /* *************************************
  * Programmer:  Sveinson
- * Class:       CS20S 0X
  * 
  * Assignment:  if-else example, calcualte membership dues
  * 
@@ -68,7 +67,7 @@
          tokens = strin.split(delim); 
           /* split does just what it sounds like, it splits the 
            * string into tokens, each token is also a string
-           * "bob"  "15"  "23
+           * "bob"  "15"  "23"
            * each token is numbered with and index stargting at 0
            * so tokens[0] is "bob", tokens[1] is "15" and tokens[2] is "23"
           */ 
@@ -76,10 +75,18 @@
          // parse input into variable
          
          // use a try catch block to catch runtime exceptions
+         try{
              name = tokens[0];                      // name is a string so no parsing is needed
              age = Integer.parseInt(tokens[1]);     // "15" must converted to 15 (String to int)
              months = Integer.parseInt(tokens[2]);  // "23" must converted to 23 (String to int) 
-                 
+            }// end try
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("input must be 3 items");
+            }// end catch array idex error
+            catch(NumberFormatException e){
+                System.out.println("all input must be int");
+            }// end catch number format error
+            
          // test input and parsing
          /*System.out.println("name: " + name);
          System.out.println("age: " + age);
@@ -89,12 +96,12 @@
          // ******* processing *******
          
          if(age < AGETHRESHOLD){
-           //System.out.println("junior"); 
-           dues = JUNIORFEE * months;           // calculate juniot dues owing 
+           //System.out.println("junior");
+           dues = JUNIORFEE * months;
          } // end if < 18
          else{
            //System.out.println("adult");
-           dues = ADULTFEE * months;           // calculate adult dues owing
+           dues = ADULTFEE * months;
          } // end  18 and over
          
          //System.out.println(dues);
